ae5a851fec9530fb411569e3ede7ed38b7076a9d
aiya000 <aiya000.develop@gmail.com>
2018-06-30 23:09:50 +0900

    :+1: Fix a representation about F-algebra
    F-algebra may not be a data,
    it may cannot represent the initial algebra.

--- FAlgebra.hs
+++ FAlgebra.hs
@@ -1,101 +1,72 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 
+-- |
+-- あるFunctor fに対する、緩いf-代数の表現と、
+-- MaybeをfとしたMaybe-代数について。
+module Main where
+
 import Control.Arrow ((>>>))
 import Data.Char (ord)
 import Data.Semigroup ((<>))
 
--- | F-代数の定義
-data FAlgebra f a = FAlgebra
-  { object :: a
-  , morph  :: f a -> a
-  }
-
--- | あるMaybe-代数aを表す型
-type MaybeAlgebra = FAlgebra Maybe
+-- |
+-- あるfに対するf-代数(a, f a -> a)。
+-- 表現の都合上a -> fの依存性があるが、本質には関係ない。
+class Functor f => FAlgebra f a | a -> f where
+  raw  :: a        -- ^ あるaの値（なんでもいい）
+  down :: f a -> a -- ^ あるf a -> aの値（なんでもいい）
 
 -- | (Maybe Char -> Char, Char)のMaybe-代数
-fMaybeChar :: MaybeAlgebra Char
-fMaybeChar = FAlgebra
-              { object = 'a'
-              , morph  = f
-              }
-  where
-    f = \case
-      Nothing -> '0'
-      Just a  -> a
+instance FAlgebra Maybe Int where
+  raw = 10
+  down (Just x) = x
+  down Nothing  = 20
 
--- | (Maybe Int -> Int, Int)のMaybe-代数
-fMaybeInt :: MaybeAlgebra Int
-fMaybeInt = FAlgebra
-             { object = 10
-             , morph  = f
-             }
-  where
-    f = \case
-      Nothing -> 0
-      Just a  -> a
+-- | (Maybe Char -> Char, Char)のMaybe-代数
+instance FAlgebra Maybe Char where
+  raw = 'a'
+  down (Just x) = x
+  down Nothing  = 'x'
 
--- |
--- あるFunctor f, a, bに対して
--- FAlgebra f aからFAlgebra f bへの準同型写像
+-- | f-代数aからf-代数bへの準同型写像
 data FHomo f a b = FHomo
   { higher :: f a -> f b
   , lower  :: a -> b
   }
 
--- |
--- スマートコンストラクタ。
--- 準同型写像はある`Functor f`と`a -> b`から定義される
-homo :: Functor f => (a -> b) -> FHomo f a b
-homo f = FHomo
-          { higher = fmap f
-          , lower  = f
-          }
+-- | 準同型写像はある`Functor f`と`a -> b`から定義できる
+fhomo :: Functor f => (a -> b) -> FHomo f a b
+fhomo f = FHomo
+            { higher = fmap f
+            , lower  = f
+            }
 
--- | MaybeAlgebra aとMaybeAlgebra bへの間の準同型写像を表す型
-type MaybeHomo a b = FHomo Maybe a b
-
--- |
--- fmap fと合わせて
--- MaybeAlgebra Char から MaybeAlgebra Int への準同型
-homoCharInt :: MaybeHomo Char Int
-homoCharInt = homo f
+-- | Maybe-代数Char から Maybe-代数Int への準同型
+homoCharToInt :: FHomo Maybe Char Int
+homoCharToInt = fhomo f
   where
     f :: Char -> Int
     f = ord
 
-type a ~~> b = MaybeHomo a b
-
--- homoCharInt :: Char ~~> Int
--- homoCharInt = ...
-
 -- |
 -- `FHomo f a b`の満たすべき法則
 -- （Haskell上で確認するために、特別にEq制約を追加）
-homoLaw :: forall f a b. (Functor f, Eq b)
+homoLaw :: forall f a b. (FAlgebra f a, FAlgebra f b, Eq b)
             => FHomo f a b  -- ^ 検査の対象
-            -> f a          -- ^ 始点
-            -> FAlgebra f a -- ^ `f a -> a`
-            -> FAlgebra f b -- ^ `f b -> b`
-            -> Bool
-homoLaw FHomo{..} fa FAlgebra{morph = downToA} FAlgebra{morph = downToB} =
-  let overWay  = lower . downToA  :: f a -> b
-      underWay = downToB . higher :: f a -> b
+              -> f a -- ^ 始点
+              -> Bool
+homoLaw FHomo{..} fa =
+  let overWay  = lower . down   :: f a -> b
+      underWay = down  . higher :: f a -> b
   in overWay fa == underWay fa
 
--- | Maybe-代数への特殊化
-maybeHomoLaw :: Eq b
-                  => MaybeHomo a b -> Maybe a
-                  -> MaybeAlgebra a -> MaybeAlgebra b
-                  -> Bool
-maybeHomoLaw = homoLaw
-
 main :: IO ()
-main = putStrLn $ "is homoCharInt a homomorphism?: " <> show isHomoCharIntHomo
+main = putStrLn $ "is homoCharToInt a homomorphism?: " <> show isHomoCharIntHomo
   where
     isHomoCharIntHomo :: Bool
-    isHomoCharIntHomo = homoLaw homoCharInt (Just 'a') fMaybeChar fMaybeInt
+    isHomoCharIntHomo = homoLaw homoCharToInt (Just 'a')
